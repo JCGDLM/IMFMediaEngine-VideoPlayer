bd6125f3558782dd78199eb93925ab24f779a846
JCGDLM <jcguimet@sfr.fr>
2026-08-31 18:47:16 +0200

    Refactor: Add Direct3D 11 and DXGI support for video rendering

--- IMFMediaEngine-VideoPlayer/VideoPlayer.h
+++ IMFMediaEngine-VideoPlayer/VideoPlayer.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <windows.h>
+#include <d3d11.h>
+#include <dxgi.h>
 #include <mfapi.h>
 #include <mfidl.h>
 #include <mfmediaengine.h>
@@ -13,6 +15,8 @@
 #pragma comment(lib, "mfplat.lib")
 #pragma comment(lib, "ole32.lib")
 #pragma comment(lib, "oleaut32.lib")
+#pragma comment(lib, "d3d11.lib")
+#pragma comment(lib, "dxgi.lib")
 
 using Microsoft::WRL::ComPtr;
 
@@ -41,12 +45,25 @@ public:
     HRESULT SetVolume(double volume); // 0.0 à 1.0
     double GetVolume() const;
     
+    // Rendu
+    HRESULT Render();
+    
     // Nettoyage
     void Shutdown();
 
 private:
+    // Interfaces COM
     ComPtr<IMFMediaEngine> m_spMediaEngine;
+    ComPtr<ID3D11Device> m_spD3D11Device;
+    ComPtr<ID3D11DeviceContext> m_spD3D11Context;
+    ComPtr<IDXGISwapChain> m_spSwapChain;
+    ComPtr<ID3D11RenderTargetView> m_spRenderTargetView;
+    
+    // État
     HWND m_hwndVideo;
     double m_volume;
     BOOL m_isPlaying;
+    
+    // Méthodes privées
+    HRESULT InitializeDirect3D();
 };
